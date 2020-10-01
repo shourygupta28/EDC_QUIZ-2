ec29a168006dd51e51bf01e654c3570c0d9f7c2c
shourygupta28 <54794237+shourygupta28@users.noreply.github.com>
2020-10-02 02:38:06 +0530

    UI/UX improved #11

--- questions/views.py
+++ questions/views.py
@@ -7,6 +7,11 @@ import datetime
 
 @login_required
 def question(request):
+    ans=Answer.objects.all()
+    for a in ans:
+        if request.user == a.candidate :
+            return render(request, 'home/ThankYou.html')
+            break
     if not (request.user.start_time):
         u = request.user
         u.start_time = datetime.datetime.now()
@@ -25,7 +30,7 @@ def question(request):
     context = { 
         'form': AnswerForm(),
         'questions': ques,
-        'end_time': (request.user.start_time + datetime.timedelta(minutes=15)).strftime("%B %d, %Y %H:%M:%S")
+        'end_time': (request.user.start_time + datetime.timedelta(minutes=1)).strftime("%B %d, %Y %H:%M:%S")
     }
     
     print(request.user.start_time, context['end_time'])
